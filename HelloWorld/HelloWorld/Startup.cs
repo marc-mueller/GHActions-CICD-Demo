8cb4759c55927b129dab177421366e49cd937036
Marc Müller <marc.mueller@4tecture.ch>
2022-03-29 11:33:43 +0200

    initial apps and workflows

---
+++ HelloWorld/HelloWorld/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWorld.Options;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HelloWorld
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(env.ContentRootPath)
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+              .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddSingleton<ConfigurationOptions>(new ConfigurationOptions() { Message = Configuration["Message"], DeploymentEnvironment = Configuration["DeploymentEnvironment"] });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# GHActions-CIDD-Demo
+# GitHub Actions Demo
+
+## Setup
+
+Perform the following action to setup resource groups and secrets fo the GitHub Actions Workflows
+
+- Create a resource group
+
+  ```
+  az group create -n demo-gh-actions -l westeurope
+  ```
+
+- Create a service principal
+
+  ```
+  az ad sp create-for-rbac --name gh-actions-demo --role contributor --scopes /subscriptions/{subscription-id}/resourceGroups/demo-gh-actions --sdk-auth
+  ```
+
+- Create secrets in GitHub repository
+  - AZURE_CREDENTIALS --> paste the entire json from the command above
+  - AZURE_RG --> resource group name
+  - AZURE_SUBSCRIPTION --> subscription id
